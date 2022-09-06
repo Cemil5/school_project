04a1280c348a5832f5a1e5ea5a101ae8668e9da8
khashayar shayesteh <shayesteh.office@gmail.com>
2022-09-05 22:59:22 -0700

    first parent sprint done

--- src/main/java/com/cydeo/service/ParentService.java
+++ src/main/java/com/cydeo/service/ParentService.java
@@ -11,48 +11,34 @@ import java.util.stream.Collectors;
 import static com.cydeo.database.Database.parentList;
 import static com.cydeo.database.Database.schoolList;
 
-public class ParentService implements CRUDService<Parent>{
+public class ParentService implements CRUDService<Parent> {
     @Override
     public Parent findById(int id) {
-        return parentList.stream().filter(parent -> parent.getId()==id)
+        return parentList.stream().filter(parent -> parent.getId() == id)
                 .findAny().get();
     }
 
     @Override
     public List<Parent> findAll() {
-        return new ArrayList<>(parentList);
-
+        return parentList;
     }
 
     @Override
     public void save(Parent parent) {
-
         parentList.add(parent);
-
     }
 
     @Override
     public void update(Parent parent) {
-
-
-
-
-
         for (int i = 0; i < parentList.size(); i++) {
-            if ( parentList.get(i).getId()== parent.getId()){
-                parentList.set(i,parent);
+            if (parentList.get(i).getId() == parent.getId()) {
+                parentList.set(i, parent);
             }
         }
-
-
-
-
-
     }
 
     @Override
     public void deleteById(int id) {
-        parentList.removeIf(P->P.getId()==id);
-
+        parentList.removeIf(p -> p.getId() == id);
     }
 }
