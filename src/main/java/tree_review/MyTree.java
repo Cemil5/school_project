3f6cb5f414fcec38b92721f331d0c9b0d7b759d9
gadiribrahimov <iqq87@yahoo.com>
2022-11-09 09:41:35 -0800

    method insert and preOrderTraversal

--- src/main/java/tree_review/MyTree.java
+++ src/main/java/tree_review/MyTree.java
@@ -1,15 +1,44 @@
 package tree_review;
 
+import lombok.NoArgsConstructor;
+
 public class MyTree {
     Node root;
 
     void insert(int value){
+Node node = new Node(value);
+if (root == null){
+    root = node;
+    return;
+}
+        Node current = root;
+while (true){
+    if (value <= current.value){
+
+        if (current.leftChild == null){
+            current.leftChild = node;
+            break;
+        }
+        current = current.leftChild;
+    }else{
+        if (current.rightChild == null){
+            current.rightChild = node;
+            break;
+        }
+        current = current.rightChild;
+    }
+}
 
     }
 
     // Root-Left-Right
     void preOrderTraversal(Node root){
-
+        if (root == null){
+            return;
+        }
+        System.out.println(root.value);
+        preOrderTraversal(root.leftChild);
+        preOrderTraversal(root.rightChild);
     }
 
     void inOrderTraversal(Node root){
