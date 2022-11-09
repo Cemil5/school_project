a462f11fa8fdb3d460bbef75a1e1fda3784173e1
Cemil <chrisw@cydeo.com>
2022-11-09 20:15:52 +0300

    tree practice

---
+++ src/main/java/tree_review/MyTree.java
@@ -0,0 +1,26 @@
+package tree_review;
+
+public class MyTree {
+    Node root;
+
+    void insert(int value){
+
+    }
+
+    // Root-Left-Right
+    void preOrderTraversal(Node root){
+
+    }
+
+    void inOrderTraversal(Node root){
+
+    }
+
+    void postOrderTraversal(Node root){
+
+    }
+
+    void levelOrderTraversal(){
+
+    }
+}
