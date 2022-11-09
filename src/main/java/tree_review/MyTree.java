a081f4a593377783cef6c328317cc8d95edfa69b
yasinTawasulli <royesh14@gmail.com>
2022-11-09 09:58:32 -0800

    pre/post/in Order completed

--- src/main/java/tree_review/MyTree.java
+++ src/main/java/tree_review/MyTree.java
@@ -2,6 +2,9 @@ package tree_review;
 
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class MyTree {
     Node root;
 
@@ -42,14 +45,42 @@ while (true){
     }
 
     void inOrderTraversal(Node root){
-
+        if (root == null){
+            return;
+        }
+        inOrderTraversal(root.leftChild);
+        System.out.println(root.value);
+        inOrderTraversal(root.rightChild);
     }
 
     void postOrderTraversal(Node root){
 
+        if (root == null){
+            return;
+        }
+        postOrderTraversal(root.leftChild);
+        postOrderTraversal(root.rightChild);
+        System.out.println(root.value);
+
     }
 
     void levelOrderTraversal(){
+        if (root == null){
+            return;
+        }
+        Queue<Node> queue=new ArrayDeque<>();
+
+        queue.add(root);
+        while (!queue.isEmpty()){
+            Node node=queue.poll();
+            System.out.println(node.value);
+            if(node.leftChild !=null){
+                queue.add(node.leftChild);
+            }
+            if(node.rightChild !=null){
+                queue.add((node.rightChild));
+            }
+        }
 
     }
 }
