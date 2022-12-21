39c0a1b2edb8c1f40cdddc8e2e1821e59ffc470b
Cemil <chrisw@cydeo.com>
2022-12-21 21:11:12 +0300

    added create and delete method

--- src/main/java/com/cydeo/git_github_practice/UserServiceImpl.java
+++ src/main/java/com/cydeo/git_github_practice/UserServiceImpl.java
@@ -1,4 +1,14 @@
 package com.cydeo.git_github_practice;
 
 public class UserServiceImpl {
+    public void create(){
+        System.out.println("test");
+    }
+    public void delete(){
+//Burulai
+    }
+
+    public void oneMoreMethod(){
+        System.out.println("jawid");
+    }
 }
